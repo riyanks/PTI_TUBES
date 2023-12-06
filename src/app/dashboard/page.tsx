f269408561e28cce579b505afe6f047331f5c5f0
riyanks <riyankhoirisiregar1215@gmail.com>
2023-12-06 08:39:49 +0700

    new config

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,13 +4,13 @@ import { redirect } from "next/navigation";
 import Navbar from "@/components/ui/Navbar";
 import dynamic from "next/dynamic";
 
-const Map = dynamic(
-  () => import('@/components/ui/Map'),
-  { 
-    loading: () => <p>A map is loading</p>,
-    ssr: false
-  }
-)
+// const Map = dynamic(
+//   () => import('@/components/ui/Map'),
+//   { 
+//     loading: () => <p>A map is loading</p>,
+//     ssr: false
+//   }
+// )
 
 const Dashboard = async () => {
   const session = await getServerSession();
@@ -24,7 +24,6 @@ const Dashboard = async () => {
       <span className="font-bold text-4xl">Dashboard</span>
 
       <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg">
-      <Map />
       </div>
       <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
     </div>
--- src/components/ui/Map.tsx
+++ src/components/ui/Map.tsx
@@ -6,7 +6,7 @@ import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
-export default function MyMap(props: any) {
+export default function Map(props: any) {
   const { position, zoom } = props
 
   return <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
